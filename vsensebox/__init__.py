e4b314661f1703fe477adc3736e926ec03e29e04
Ratha SIV <60795657+rathaumons@users.noreply.github.com>
2026-05-26 13:54:20 +0200

    Bump version from 0.4.1 to 0.5.0

--- vsensebox/__init__.py
+++ vsensebox/__init__.py
@@ -7,7 +7,7 @@ from vsensebox.vsense import VSense
 from vsensebox.gui import config, reset
 from vsensebox.utils.about import docs, github
 
-__version__ = "0.4.1"
+__version__ = "0.5.0"
 __author__ = "Ratha SIV"
 __description__ = "VSenseBox - Python toolbox for visual sensing."
 __homepage__ = "https://numediart.github.io/vsensebox"
@@ -21,4 +21,3 @@ __all__ = (
     "docs", 
     "github"
 )
-
